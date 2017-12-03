635042ea268c5699a3e77be7a831e3e375807bf7
ryantetland <rmt378@mail.usask.ca>
2017-12-03 15:59:31 -0600

    change names of functions

--- unspent_transactions.py
+++ unspent_transactions.py
@@ -12,18 +12,19 @@ from typing import (Mapping)
 
 utxo_set: Mapping[OutPoint, UnspentTxOut] = {}
 
-def add_to_unspent_txs(tx_out, tx, idx, is_coinbase, height):
+def add_to_utxo(tx_out, tx, idx, is_coinbase, height):
 	utxo = UnspentTxOut(
         *txout,
         txid=tx.id, txout_idx=idx, is_coinbase=is_coinbase, height=height)
 
     logger.info(f'adding tx outpoint {utxo.outpoint} to utxo_set')
+    utxo_set[utxo.outpoint] = utxo
 
-def remove_from_unspent_txs(txid, txout_idx):
+def rm_from_utxo(txid, txout_idx):
 	del utxo_set[OutPoint(txid, txout_idx)]
 
 
-def find_unspent_txn_in_list(txin, txns) -> UnspentTxOut:
+def find_utxo_in_list(txin, txns) -> UnspentTxOut:
 	txid, txout_idx = txin.to_spend
 
 	try:
