504f7518ac899d85cc8ae1c175a261c9d74291fc
ryantetland <rmt378@mail.usask.ca>
2017-12-03 14:19:53 -0600

    more functions implemented and bug fixes

--- proof_of_work.py
+++ proof_of_work.py
@@ -36,13 +36,18 @@ class Params:
 	DIFFICULTY_PERIOD_IN_BLOCKS = (DIFFICULTY_PERIOD / DESIRED_SECONDS_BETWEEN_BLOCKS)
 
 
+	# cut mining subsidy in half after this many blocks
+	CUT_SUBSIDY_AFTER_THIS_MANY_BLOCKS = 210_000
+
+	COIN_VALUE = 100_000_000
+
 
 def get_difficulty_of_problem(prev_block_hash: str) -> int:
 	"""
 	get the difficulty that must be solved for the next block
 	"""
 
-	if prev_block_hash = "":
+	if prev_block_hash == "":
 		return Params.INITIAL_DIFFICULTY_BITS
 
 
@@ -55,10 +60,10 @@ def get_difficulty_of_problem(prev_block_hash: str) -> int:
 			this_block = active_chain[0]
 
 
-	prev_block_time_taken_to_solve = prev_block.timestamp - this_block.timestamp
+	prev_block_time_solve = prev_block.timestamp - this_block.timestamp
 
 	# if it took less time than targeted, increase difficulty
-	if prev_block_time_taken_to_solve < Params.DIFFICULTY_PERIOD
+	if prev_block_time_to_solve < Params.DIFFICULTY_PERIOD:
 		return prev_block.bits + 1
 	else: #increase difficulty
 		return prev_block.bits - 1
@@ -102,7 +107,7 @@ def construct_block(pay_to_addr, txns=None):
 
     return mine(block)
 
-"""
+
 def calculate_fees(block) -> int:
 	#the reward given to the miner
 
@@ -110,19 +115,58 @@ def calculate_fees(block) -> int:
 	spent = 0
 	sent = 0
 
-	for txn in block.txns:
-		spent = 
 
-	fee = spent - sent
+
+	def utxo_from_block(txin):
+        tx = [t.txouts for t in block.txns if t.id == txin.to_spend.txid]
+        return tx[0][txin.to_spend.txout_idx] if tx else None
+
+    def find_utxo(txin):
+        return utxo_set.get(txin.to_spend) or utxo_from_block(txin)
+
+    for txn in block.txns:
+        spent = sum(find_utxo(i).value for i in txn.txins)
+        sent = sum(o.value for o in txn.txouts)
+        fee += (spent - sent)
+
 	return fee
 
 
 
 def get_block_subsidy() -> int:
+	# more rewards to miner
 
+	if len(active_chain) // Params.CUT_SUBSIDY_AFTER_THIS_MANY_BLOCKS > 63:
+		return 0
+	else:
+		return Params.COIN_VALUE // 2**(len(active_chain) // Params.CUT_SUBSIDY_AFTER_THIS_MANY_BLOCKS)
+
+
+stop_mining = threading.Event()
 
 def mine(block):
+	start_time = time.time()
+	nonce = 0
+	difficulty_target = (1<<(256 - block.bits))
+	stop_mining.clear()
+
+	while sha256(block.header(nonce)) > target:
+		nonce = nonce + 1
+		if nonce % 10000 == 0 and stop_mining.is_set():
+			stop_mining.clear()
+			return None
+
+	block = block._replace(nonce=nonce)
+	time_elapsed = time.time() - start or 0.01
+
+	return block
 
 
 def mine_forever():
-"""
+	addr = init_wallet()[2]
+	while True:
+		block = construct_block(addr)
+
+		if block:
+			connect_block(block)
+			save_to_disk()
