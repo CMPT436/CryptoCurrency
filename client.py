868e6d4e7c739bad4c3ca07933b470ca97f8a663
Ethan Houston <ejh377@drone21.usask.ca>
2017-12-03 23:30:43 -0600

    Added barebones, GUI Skeleton for the client, for real this time

--- client.py
+++ client.py
@@ -1,1 +1,81 @@
+#Hustone
+#436 Project
+#Client GUI Class
+#*********************
+# - A GUI for Client of Crypto Implementation
+# Has functionality for seeing Balance and making transactions, and viewing tr 
 
+
+
+
+import Tkinter as tk
+import tkMessageBox
+
+
+#Balance Button PopUp
+def bal():
+	tkMessageBox.showinfo("Your Balance", "Your Balance is: "+get_bal)
+
+#Send Coin	
+def sendit():
+	tkMessageBox.showinfo("Sending", "You Sent :"+sendAmount.get()+" to Address: "+sendAddy.get())
+	
+#View Transaction	Status
+def status():
+	tkMessageBox.showinfo("Your Balance", "Your Balance is: "+get_bal)
+
+get_bal = "100"
+
+
+
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.grid()
+
+sendAmount = tk.StringVar()
+sendAddy = tk.StringVar()
+txID = tk.StringVar()
+
+button = tk.Button(frame, 
+                   text="QUIT", 
+                   fg="red",
+                   command=quit)
+button.grid(row=5, column=0, sticky=tk.W)
+
+bal_butt = tk.Button(frame,
+                   text="Balance",
+                   command=bal)
+bal_butt.grid(row=0, column=0, sticky=tk.W)
+
+send_butt = tk.Button(frame,
+                   text="Send",
+                   command=sendit)
+send_butt.grid(row=1, column=0, sticky=tk.W)
+
+amountLabel = tk.Label(frame, text="Amount: ")
+amountLabel.grid(row=1, column=1)
+
+send_AmountEntry = tk.Entry(frame, textvariable=sendAmount)
+send_AmountEntry.grid(row=1, column=2)
+
+addyLabel = tk.Label(frame, text="Address: ")
+addyLabel.grid(row=1, column=3)
+
+
+send_AddyEntry = tk.Entry(frame, textvariable=sendAddy)
+send_AddyEntry.grid(row=1, column=4)
+
+
+stat_butt = tk.Button(frame,
+                   text="View Status",
+                   command=status)
+stat_butt.grid(row=3, column=0, sticky=tk.W)
+
+txLabel = tk.Label(frame, text="Address: ")
+txLabel.grid(row=3, column=1)
+
+
+stat_TxEntry = tk.Entry(frame, textvariable=txID)
+stat_TxEntry.grid(row=3, column=2)
+
+root.mainloop()
